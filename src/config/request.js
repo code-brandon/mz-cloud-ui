e1e69e6152b9d3de980e4978434ee0ae461494b3
code-brandon <2459754464@qq.com>
2022-09-30 01:08:25 +0800

    fix:Axios响应拦截进行消息提示，路由跳转

--- src/config/request.js
+++ src/config/request.js
@@ -1,6 +1,8 @@
 import Axios from 'axios'
 import Config from './ENV.js'
 import store from '@/store'
+import router from '@/router'
+
 import {Notification, MessageBox, Message, Loading} from 'element-ui'
 
 const baseUrl = process.env.NODE_ENV === "development" ? Config.baseUrl.dev : Config.baseUrl.pro
@@ -55,9 +57,56 @@ class HttpRequest {
 
     // 添加响应拦截器
     instance.interceptors.response.use(function (response) {
+
+      if (response.data.code > 15000 && response.data.code < 20000 ) {
+
+        if (response.data.code === 15004 && !response.config.url.includes('oauth/token','login','auth/logout')) {
+          MessageBox.alert(response.data.message, "登录失效", {
+            confirmButtonText: "跳转登录页面",
+            callback: action => {
+              // 跳转登录页
+              store.commit('CLEAR_TOKEN')
+              store.commit('CLEAR_USER')
+              router.push('/login')
+            }
+          });
+        } else {
+          Message.error(response.data.message ? response.data.message : "用户登录状态失效");
+        }
+
+      }else if (response.status === 200) {
+        if (response.data.code === 1) {
+          Message({
+            title: "错误",
+            message: response.data.message,
+            type: "error"
+          });
+
+        }
+        if (response.data.code === 0) {
+          /*Message({
+            title: "成功",
+            message: `操作成功`,
+            type: "success"
+          });*/
+          Notification({
+            title: '成功',
+            message: '这是一条成功的提示消息',
+            type: 'success'
+          });
+
+        }
+      }
       // 对响应数据做些什么
       return response;
     }, function (error) {
+      if (error.response.status == 401) {
+        Message({
+          title: "警告",
+          message: error.response.data.message,
+          type: "warning"
+        });
+      }
       // 处理响应错误
       return Promise.reject(error);
     });
