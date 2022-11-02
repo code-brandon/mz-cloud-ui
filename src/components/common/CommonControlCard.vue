83e232edec54e702c9eaed91c811f855040a8d37
code-brandon <2459754464@qq.com>
2022-11-03 00:59:06 +0800

    feat：添加公共组件

---
+++ src/components/common/CommonControlCard.vue
@@ -0,0 +1,137 @@
+<!--
+  功能：功能描述
+  作者：小政同学
+  邮箱：1911298402@qq.com
+  时间：2022/10/11 10:56
+  版本：v1.0
+  修改记录：
+  修改内容：
+  修改人员：
+  修改时间：
+-->
+<template>
+  <el-row type="flex" class="row-control" justify="space-between">
+    <el-col :span="20" class="col-left">
+      <slot></slot>
+    </el-col>
+    <el-col :span="4" class="col-right">
+      <el-tooltip :content="'隐藏搜搜'" placement="top">
+        <el-button icon="el-icon-search" circle size="mini" @click="handleSearch"></el-button>
+      </el-tooltip>
+      <el-tooltip :content="'刷新'" placement="top">
+        <el-button icon="el-icon-refresh" circle size="mini" @click="handleRefresh"></el-button>
+      </el-tooltip>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  // 组件名称
+  name: 'CommonControlCard',
+  // 组件参数 接收来自父组件的数据
+  props: {},
+  // 局部注册的组件
+  components: {},
+  // 组件状态值
+  data() {
+    return {}
+  },
+  // 组件方法
+  methods: {
+    handleSearch(row) {
+      this.$emit('search', row)
+    },
+    handleRefresh(row) {
+      this.$emit('refresh', row)
+    },
+  },
+  // 计算属性
+  computed: {},
+  // 侦听器
+  watch: {},
+  // 以下是生命周期钩子   注：没用到的钩子请自行删除
+  /**
+   * 在实例初始化之后，组件属性计算之前，如data属性等
+   */
+  beforeCreate() {
+
+  },
+  /**
+   * 组件实例创建完成，属性已绑定，但DOM还未生成，$ el属性还不存在
+   */
+  created() {
+
+  },
+  /**
+   * 在挂载开始之前被调用：相关的 render 函数首次被调用。
+   */
+  beforeMount() {
+
+  },
+  /**
+   * el 被新创建的 vm.$ el 替换，并挂载到实例上去之后调用该钩子。
+   * 如果 root 实例挂载了一个文档内元素，当 mounted 被调用时 vm.$ el 也在文档内。
+   */
+  mounted() {
+
+  },
+  /**
+   * 数据更新时调用，发生在虚拟 DOM 重新渲染和打补丁之前。
+   * 你可以在这个钩子中进一步地更改状态，这不会触发附加的重渲染过程。
+   */
+  beforeUpdate() {
+
+  },
+  /**
+   * 由于数据更改导致的虚拟 DOM 重新渲染和打补丁，在这之后会调用该钩子。
+   * 当这个钩子被调用时，组件 DOM 已经更新，所以你现在可以执行依赖于 DOM 的操作。
+   */
+  updated() {
+
+  },
+  /**
+   * keep-alive 组件激活时调用。 仅针对keep-alive 组件有效
+   */
+  activated() {
+
+  },
+  /**
+   * keep-alive 组件停用时调用。 仅针对keep-alive 组件有效
+   */
+  deactivated() {
+
+  },
+  /**
+   * 实例销毁之前调用。在这一步，实例仍然完全可用。
+   */
+  beforeDestroy() {
+
+  },
+  /**
+   * Vue 实例销毁后调用。调用后，Vue 实例指示的所有东西都会解绑定，
+   * 所有的事件监听器会被移除，所有的子实例也会被销毁。
+   */
+  destroyed() {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!--使用了scoped属性之后，父组件的style样式将不会渗透到子组件中，-->
+<!--然而子组件的根节点元素会同时被设置了scoped的父css样式和设置了scoped的子css样式影响，-->
+<!--这么设计的目的是父组件可以对子组件根元素进行布局。-->
+<style scoped lang="less">
+.row-control {
+  margin: 10px;
+
+  .col-left {
+    text-align: left;
+  }
+
+  .col-right {
+    text-align: right;
+  }
+}
+</style>
--- src/components/common/CommonTable.vue
+++ src/components/common/CommonTable.vue
@@ -1,17 +1,10 @@
 <template>
   <div class="common-table">
-    <el-table
-            :data="tableData"
-            height="90%"
-            stripe>
-      <el-table-column
-              show-overflow-tooltip
-              v-for="item in tableLabel"
-              :key="item.prop"
-              :label="item.label"
-              :width="item.width ? item.width : 125">
+    <el-table :data="tableData" height="90%" stripe>
+      <el-table-column show-overflow-tooltip v-for="item in tableLabel" :key="item.prop" :label="item.label"
+        :width="item.width ? item.width : 125">
         <template slot-scope="scope">
-          <span style="margin-left: 10px">{{scope.row[item.prop]}}</span>
+          <span style="margin-left: 10px">{{ scope.row[item.prop] }}</span>
         </template>
       </el-table-column>
       <el-table-column label="操作" min-width="180">
@@ -21,47 +14,41 @@
         </template>
       </el-table-column>
     </el-table>
-    <el-pagination
-            @size-change="changeSize"
-            @current-change="changePage"
-            :current-page="tableData.currPage"
-            :page-sizes="[10, 20, 30, 40]"
-            :page-size="tableData.pageSize"
-            layout="total, sizes, prev, pager, next, jumper"
-            background
-            :total="tableData.totalCount">
+    <el-pagination @size-change="changeSize" @current-change="changePage" :current-page="tableData.currPage"
+      :page-sizes="[10, 20, 30, 40]" :page-size="tableData.pageSize" layout="total, sizes, prev, pager, next, jumper"
+      background :total="tableData.totalCount">
     </el-pagination>
   </div>
 </template>
 
 <script>
-    export default {
-        name: "CommonTable",
-        props:{
-            tableData: Array,
-            tableLabel: Array,
-            config:Object
-        },
-        data() {
-            return {
+export default {
+  name: "CommonTable",
+  props: {
+    tableData: Array,
+    tableLabel: Array,
+    config: Object
+  },
+  data() {
+    return {
 
-            }
-        },
-        methods:{
-            handleEdit(row){
-              this.$emit('edit',row)
-            },
-            handleDelete(row){
-                this.$emit('del',row)
-            },
-            changePage(page){
-                this.$emit('changePage',page)
-            },
-            changeSize(size){
-                this.$emit('changePage',size)
-            }
-        }
     }
+  },
+  methods: {
+    handleEdit(row) {
+      this.$emit('edit', row)
+    },
+    handleDelete(row) {
+      this.$emit('del', row)
+    },
+    changePage(page) {
+      this.$emit('changePage', page)
+    },
+    changeSize(size) {
+      this.$emit('changePage', size)
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
@@ -69,6 +56,7 @@
   height: calc(100% - 60px);
   background-color: #fff;
   position: relative;
+
   .pager {
     position: absolute;
     bottom: 0;
